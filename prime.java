1c8bfbb805be3c44b686201743cabb96e1e5e604
Pavitra Gouroji <pavitragouriji@gmail.com>
2026-09-25 19:34:21 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,11 @@
+class prime
+{
+    public static void main(String a[])
+    {
+       int i=97;
+       if(i%1==0 && i%97==0)
+        System.out.println("given number is prime");
+       else
+        System.out.println("given number is not prime");
+    }
+}
